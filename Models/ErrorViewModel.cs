d1acd07786c2e2b773d0586bae720d1a751f7524
Teodor <sesumt@rocketmail.com>
2025-01-30 17:46:35 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BibliotekaPPP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
