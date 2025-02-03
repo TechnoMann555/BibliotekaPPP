01ed6500d72e2468bbdafba28f944c29bcca0fa9
Teodor <sesumt@rocketmail.com>
2025-02-03 10:09:13 +0100

    Implementirao funkcionalnost pregleda licnih i clanskih podataka za clana.

--- Models/EFRepository/ClanRepository.cs
+++ Models/EFRepository/ClanRepository.cs
@@ -7,6 +7,50 @@ namespace BibliotekaPPP.Models.EFRepository
 {
     public class ClanRepository : IClanRepository
     {
-        
+        private BibliotekaContext bibliotekaContext = new BibliotekaContext();
+
+        #region Metode za prevodjenje tipova
+
+        private async Task<ClanBO> ConvertClanToClanBO(Clan clan, NalogBO? korisnickiNalog = null)
+        {
+            ClanBO clanBO = new ClanBO()
+            {
+                ClanId = clan.ClanId,
+                Jcb = clan.Jcb,
+                DatumUclanjenja = clan.DatumUclanjenja,
+                BrLicneKarte = clan.BrLicneKarte,
+                ImePrezime = clan.ImePrezime,
+                DatumRodjenja = clan.DatumRodjenja,
+                ImeRoditelja = clan.ImeRoditelja,
+                AdresaStanovanja = clan.AdresaStanovanja,
+                Zanimanje = clan.Zanimanje,
+                KontaktTelefon = clan.KontaktTelefon,
+                KontaktMejl = clan.KontaktMejl
+            };
+
+            if(korisnickiNalog != null)
+                clanBO.KorisnickiNalog = korisnickiNalog;
+            else
+            {
+                Nalog? nalog = await bibliotekaContext.Nalogs.FirstOrDefaultAsync(n => n.NalogId == clan.KorisnickiNalogFk);
+                clanBO.KorisnickiNalog = (nalog == null) ? null : new NalogBO(nalog);
+            }
+
+            return clanBO;
+        }
+
+        #endregion
+
+        public async Task<ClanBO?> TraziClanaPoNalogID(int nalogID)
+        {
+            Nalog? korisnickiNalog = await bibliotekaContext.Nalogs.Include(n => n.Clan).FirstOrDefaultAsync(n => n.NalogId == nalogID);
+
+            if(korisnickiNalog == null)
+                return null;
+
+            ClanBO nadjenClan = await ConvertClanToClanBO(korisnickiNalog.Clan, new NalogBO(korisnickiNalog));
+
+            return nadjenClan;
+        }
     }
 }
